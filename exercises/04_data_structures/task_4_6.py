d0dd5e8d1e26c760ae237c0638acd134e7a68da2
bycha <bycha.justbahbka@gmail.com>
2023-01-11 05:04:40 -0600

    daily

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -33,10 +33,10 @@ ospf_route = "10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
 ospf_route = ospf_route.replace(',',' ').replace('[','').replace(']','').split()
 
 output = """
-Prefix,             {}
-AD/Metric,          {}
-Next-Hop,           {}
-Last update,        {}
+Prefix              {}
+AD/Metric           {}
+Next-Hop            {}
+Last update         {}
 Outbound Interface  {}
 """
 
