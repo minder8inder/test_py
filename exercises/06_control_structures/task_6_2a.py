1868ee1afd9b38f90381d027a6afccd87fcf51b5
minder8inder <121382466+minder8inder@users.noreply.github.com>
2023-02-01 12:25:51 +0400

    Update task_6_2a.py
    Daily

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,30 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip = input('Введите IP-адрес:\n')
+ip_oct = ip.split('.')
+right = True
+
+if len(ip_oct) != 4:
+    right = False
+else:
+    for i in ip_oct:
+        if i.isdigit() != True or int(i) not in range(256):
+            right = False
+            break
+
+                
+if right == False:
+    print('Неправильный IP-адрес')
+else:
+    if 1 <= int(ip_oct[0]) <= 223:
+        print('unicast')
+    elif 224 <= int(ip_oct[0]) <= 239:
+        print('multicast')
+    elif ip == '255.255.255.255':
+        print('local broadcast')
+    elif ip == '0.0.0.0':
+        print('unassgined')
+    else:
+        print('unused')
