672bf7be48a2acc0c26c8bc98bb99b93977f9f7c
minder8inder <121382466+minder8inder@users.noreply.github.com>
2023-03-16 12:43:13 +0400

    Update task_9_1a.py
    Done

--- exercises/09_functions/task_9_1a.py
+++ exercises/09_functions/task_9_1a.py
@@ -28,18 +28,47 @@ print(generate_access_config(access_config, access_mode_template, port_security_
 
 """
 
+
+
 access_mode_template = [
-    "switchport mode access",
-    "switchport access vlan",
-    "switchport nonegotiate",
-    "spanning-tree portfast",
-    "spanning-tree bpduguard enable",
+    "switchport mode access", "switchport access vlan",
+    "switchport nonegotiate", "spanning-tree portfast",
+    "spanning-tree bpduguard enable"
 ]
+
+access_config = {
+    "FastEthernet0/12": 10,
+    "FastEthernet0/14": 11,
+    "FastEthernet0/16": 17
+}
+
+access_config_2 = {
+    "FastEthernet0/03": 100,
+    "FastEthernet0/07": 101,
+    "FastEthernet0/09": 107,
+}
 
 port_security_template = [
     "switchport port-security maximum 2",
     "switchport port-security violation restrict",
-    "switchport port-security",
+    "switchport port-security"
 ]
 
-access_config = {"FastEthernet0/12": 10, "FastEthernet0/14": 11, "FastEthernet0/16": 17}
+def generate_access_config(intf_vlan_mapping, access_template, psecurity=None):
+    
+    configuration = []
+    
+    for int, vlan in intf_vlan_mapping.items():
+        configuration.append(f"interface {int}")
+        for command in access_template:
+            if command.endswith("access vlan"):
+                configuration.append(f"{command} {vlan}")
+            else:
+                configuration.append(command)
+        if psecurity:
+            configuration.extend(psecurity)
+    return(configuration)
+            
+print(generate_access_config(access_config, access_mode_template))
+print(generate_access_config(access_config, access_mode_template, port_security_template))
+
